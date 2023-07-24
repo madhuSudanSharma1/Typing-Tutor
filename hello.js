d16c46032ea974dde6dc2b9b5f93084c27cb058e
Madhu Sharma <077bct042.madhu@pcampus.edu.np>
2023-07-24 16:31:54 +0545

    Level 1

--- hello.js
+++ hello.js
@@ -7,25 +7,12 @@ document.addEventListener('DOMContentLoaded', function () {
   const typingInput = document.getElementById('typingInput');
   const wordOutput = document.getElementById('wordOutput');
   const levels = {
-    level1: 'ब द ग ह ज क ल म न प र स त बद दग गह हज जक कल लम मन नप पर रस सत बदग दगह गहज हजक जकल कलम लमन मनप नपर परस रसत बदगह दगहज गहजक हजकल जकलम कलमन लमनप मनपर नपरस परसत बदगहज दगहजक गहजकल हजकलम जकलमन कलमनप लमनपर मनपरस नपरसत बदगहजक दगहजकल गहजकलम हजकलमन जकलमनप कलमनपर लमनपरस मनपरसत बदगहजकल दगहजकलम गहजकलमन हजकलमनप जकलमनपर कलमनपरस लमनपरसत बदगहजकलम दगहजकलमन गहजकलमनप हजकलमनपर जकलमनपरस कलमनपरसत बदगहजकलमन दगहजकलमनप गहजकलमनपर हजकलमनपरस जकलमनपरसत बदगहजकलमनप दगहजकलमनपर गहजकलमनपरस हजकलमनपरसत बदगहजकलमनपर दगहजकलमनपरस गहजकलमनपरसत बदगहजकलमनपरस दगहजकलमनपरसत बदगहजकलमनपरसत',
+    level1: 'बद दग गह हज जक कल लम मन नप पर रस सत बदग दगह गहज हजक जकल कलम लमन मनप नपर परस रसत बदगह दगहज गहजक हजकल जकलम कलमन लमनप मनपर नपरस परसत बदगहज दगहजक गहजकल हजकलम जकलमन कलमनप लमनपर मनपरस नपरसत बदगहजक दगहजकल गहजकलम हजकलमन जकलमनप कलमनपर लमनपरस मनपरसत बदगहजकल दगहजकलम गहजकलमन हजकलमनप जकलमनपर कलमनपरस लमनपरसत बदगहजकलम दगहजकलमन गहजकलमनप हजकलमनपर जकलमनपरस कलमनपरसत बदगहजकलमन दगहजकलमनप गहजकलमनपर हजकलमनपरस जकलमनपरसत बदगहजकलमनप दगहजकलमनपर गहजकलमनपरस हजकलमनपरसत बदगहजकलमनपर दगहजकलमनपरस गहजकलमनपरसत बदगहजकलमनपरस दगहजकलमनपरसत बदगहजकलमनपरसत',
+    // level1: 'ब द ग ह ज क ल म न प र स त बद दग गह हज जक कल लम मन नप पर रस सत बदग दगह गहज हजक जकल कलम लमन मनप नपर परस रसत बदगह दगहज गहजक हजकल जकलम कलमन लमनप मनपर नपरस परसत बदगहज दगहजक गहजकल हजकलम जकलमन कलमनप लमनपर मनपरस नपरसत बदगहजक दगहजकल गहजकलम हजकलमन जकलमनप कलमनपर लमनपरस मनपरसत बदगहजकल दगहजकलम गहजकलमन हजकलमनप जकलमनपर कलमनपरस लमनपरसत बदगहजकलम दगहजकलमन गहजकलमनप हजकलमनपर जकलमनपरस कलमनपरसत बदगहजकलमन दगहजकलमनप गहजकलमनपर हजकलमनपरस जकलमनपरसत बदगहजकलमनप दगहजकलमनपर गहजकलमनपरस हजकलमनपरसत बदगहजकलमनपर दगहजकलमनपरस गहजकलमनपरसत बदगहजकलमनपरस दगहजकलमनपरसत बदगहजकलमनपरसत',
 
-    level2: [
-      'भ', 'छ', 'ध', 'ऊ', 'घ', 'अ', 'झ', 'ख',
-      'कै', 'खै', 'धै', 'गै', 'टै', 'डै', 'रै', 'भै', 'खै', 'की', 'भी', 'धि',
-      'कठै', 'अल्छी', 'असल', 'घैला', 'छमछम', 'खीर', 'सबै',
-      'बदी', 'दगि', 'गही', 'हिज', 'जकी', 'कील', 'लिमै', 'मनी', 'नप', 'पर', 'रस', 'सतै',
-      'बदग', 'दगह', 'गहज', 'हजक', 'जकल', 'कलम', 'लमन', 'मनप', 'नपर', 'परस', 'रसत',
-      'बदगह', 'दगहज', 'गहजक', 'हजकल', 'जकलम', 'कलमन', 'लमनप', 'मनपर', 'नपरस', 'परसत',
-      'बदिगहीजै', 'दीगहिजेकै', 'गहजकील', 'हजैकलम', 'जकेलमैन', 'कीलमिनप', 'लमिनपर', 'मैनपीरस', 'नपरसत',
-      'बदगहजक', 'दगहजकल', 'गहजकलम', 'हजकलमन', 'जकलमनप', 'कलमनपर', 'लमनपरस', 'मनपरसत',
-      'बदगहजकल', 'दगहजकलम', 'गहजकलमन', 'हजकलमनप', 'जकलमनपर', 'कलमनपरस', 'लमनपरसत',
-      'बदगहजकलम', 'दगहजिकलमन', 'गहजकलमनप', 'हजकलमनपर', 'जकलमनपरस', 'कलमनपरसत',
-      'बदगहजकलम', 'दगहजकलमनप', 'गहजकलमनपर', 'हजकलमनपरस', 'जकलमनपरसत',
-      'बदगहजकलैमनप', 'दगहजकलमनपर', 'गहजकलमनपरस', 'हजकलमनपरसत',
-      'बदगहजकलमनपर', 'दगहजकलमनपरस', 'गहजकलमनपरसत',
-      'बदगहजकलमनपरस', 'दगहजकलमनपरसत',
-      'बदगहजकलमनपरसत'
-    ]
+    level2:
+      `मगज सत दस बर फलक दनक कचकच छत खतम चरण सहज नल नगर सडक शतक नयन मग कमसल धरमर भरसक समतलसतह बहस रसमय तडकभडक जतन नकल रमन बम बस पटपट पथ नवम दशमलब खटखट करण गरम शर तर कड सफल पकड सलह जठर झमझम तटगत वय खनन कपट मठ भग गढ मलहम गडबड नर डङडङ गलगल गणक गलत रबर तय चटक गमनपथ चयन शरण शहर जमघट बकस फसल गर पल लहडसनक सनद वध लत शय तल हरफ तह जलमय शपथ जब झपट बरफ पट धरन गठन गहन कठघर वर भल मरमत भडक परल कच सहमत यमक भलकट भकभक सतत टप दर डरभय रस महल भय खलक रक शशक मह हटक सफर कहर नस मत सलगम बदर बहन छल चलन वचन बचत शयन फरक छलफल लवणजल यव टक शकट बढ घर जलपथ लख रहल खलबल खरब लवण फतरफतर घटक टरटर पसल चरम तहपरत धर कलश जलथल समतल कलम वजन कसम खत कमल चनड लहड शमन हलचल नगरशहर कलकल लतपत करङ सरसर गज चहक नगद छनक भजन जलजन थकथक दल रगत बखत पतन बल जलकमल जमठ बगलस नगर डर सरल करतल लय सतह समथर गनगन ठहर कवच खबर`
+
     , level3: [
 
     ]
@@ -44,56 +31,96 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
   const words = levels[level];
-  const wordsArray = words.split(' ');
+  wordsArray = words.split(' ');
+  wordsArray = wordsArray.reduce((acc, el) => {
+    return [...acc, el, " "]
+  }, [])
+
+  const word_pad = 10;
   let currentWordIndex = 0;
   let currentCharacterIndex = 0;
   let currentWord = wordsArray[currentWordIndex];
   let nextCharacter = currentWord.charAt(currentCharacterIndex);
-  let set = wordsArray.slice(0, 5)
-  wordOutput.textContent = set.join(' ');
+  let i = 0;
+  let currentWordsToType = wordsArray.slice(i, i + word_pad)
+  wordOutput.textContent = currentWordsToType.join(' ');
+  let completedWords = 0;
   glowNextCharacter('', nextCharacter)
   let isInputCorrect = true
 
-  typingInput.addEventListener('input', function () {
+
+
+  typingInput.addEventListener('input', function (e) {
     const pressedCharacter = typingInput.value.charAt(typingInput.value.length - 1);
     const charCode = pressedCharacter.charCodeAt(0);
     if ((charCode >= 65 && charCode <= 90) || (charCode >= 97 && charCode <= 122)) {
       alert("Change Keyboard to Nepali Unicode Romanized")
-    }
-    if(pressedCharacter==='')
-    {
-      isInputCorrect=true;
+      e.preventDefault()
       return;
     }
-    
+    if (pressedCharacter === '') {
+      isInputCorrect = true;
+      return;
+    }
+
     if (pressedCharacter !== nextCharacter) {
       console.log(pressedCharacter.length)
       isInputCorrect = false;
-      typingInput.style.color='red';
+      typingInput.style.color = 'red';
       return;
     }
-    if(!isInputCorrect){
-      console.log("ोुत",typingInput.value)
-      if(typingInput.value.length===0)
-      {
+    if (!isInputCorrect) {
+      console.log("ोुत", typingInput.value)
+      if (typingInput.value.length === 0) {
         console.log(typingInput.value)
-        isInputCorrect=true;
+        isInputCorrect = true;
       }
       return;
     }
-    isInputCorrect=true;
-    typingInput.style.color='green';
+    isInputCorrect = true;
+    typingInput.style.color = 'green';
     currentCharacterIndex++;
 
     if (currentCharacterIndex >= currentWord.length) {
+      console.log("ehllo")
       currentWordIndex++;
-      typingInput.value = '';
+      typingInput.value = pressedCharacter;
       currentCharacterIndex = 0;
 
-      if (currentWordIndex < words.length) {
-        currentWord = words[currentWordIndex];
-        
-        wordOutput.textContent = currentWord;
+
+      // 
+      // let tempWords = Math.floor(Math.ceil(currentWordIndex / 2));
+      console.log(currentWordIndex)
+      if (currentWordIndex === word_pad) {
+        i += word_pad;
+        currentWordsToType = wordsArray.slice(i, i + word_pad)
+        currentWordIndex = 0;
+        console.log("cwtt", currentWordsToType)
+      }
+
+      let tempWords = currentWordIndex;
+      completedWords = currentWordsToType.slice(0, tempWords);
+      remainingWords = currentWordsToType.slice(tempWords);
+      // console.log(tempWords, " ", currentWordsToType.length - tempWords)
+      console.log(currentWordsToType)
+      console.log(completedWords)
+      console.log(remainingWords)
+      let el1 = document.createElement('div')
+      let el2 = document.createElement('div')
+      el1.style.color = "green"
+      el2.style.color = "black"
+      el1.textContent = completedWords.join(" ") + " ";
+      el2.textContent = remainingWords.join(" ")
+      wordOutput.innerHTML = "";
+      wordOutput.appendChild(el1);
+      wordOutput.appendChild(el2);
+      // 
+
+
+
+
+      if (currentWordIndex < wordsArray.length) {
+        currentWord = currentWordsToType[currentWordIndex];
         nextCharacter = currentWord.charAt(currentCharacterIndex);
       } else {
         wordOutput.textContent = 'Congratulations! You completed all the words.';
